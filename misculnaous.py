a774756ed5c398d895c7dd0ecef2a77cb634afa9
kazigoni <kazimdgoni@gmail.com>
2025-02-05 09:52:36 +0600

    learning type hint

---
+++ misculnaous.py
@@ -0,0 +1,10 @@
+# type hint
+def full_name(fast_name: str, last_name: str):
+    '''
+    In this funtion we are adding type hing e.g. fast_name: str (colorn and type of data.)
+    After typing fast_name when press Ctrl+Space we see options 
+    that not usually available without declaring 'type hint'
+
+    '''
+    fullname = fast_name.title() + " " + last_name.upper()
+    return fullname
