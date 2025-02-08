a87e9e0454900f9ded8a81b477755550d5007eae
kazigoni <kazimdgoni@gmail.com>
2025-02-08 23:23:25 +0600

    Enum is complete. Practiced by creating my own 'Person' class. Now learning 'file in path parameters'

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
 from enum import Enum
 from fastapi import FastAPI
 
-
-class ModelName(str, Enum):
-    alexnet = "alexnet"
-    resnet = "resnet"
-    lenet = "lenet"
-
-
 app = FastAPI()
 
 
@@ -45,20 +38,42 @@ async def read_user(user_id):
 ####### ENUM CLASS ###############
 # import 'enum' class. see above 'from enum import Enum'
 """
-Import Enum and create a sub-class that inherits from str and from Enum.
+Import Enum and create a sub-class that inherits from 'str' and from 'Enum'.
 
-By inheriting from str the API docs will be able to know that the values must be of 
-type string and will be able to render correctly.
+By inheriting from 'str' the API docs will be able to know that the values must be of 
+'type string' and will be able to render correctly.
 
 Then create class attributes with fixed values, which will be the available valid values:
 """
 
 
-@app.get("/machine/{models}")
-async def machinelearning_models(models):
-    if models == ModelName.alexnet:
-        return ModelName.alexnet
-    if models == ModelName.resnet:
-        return ModelName.resnet
-    if models == ModelName.lenet:
-        return ModelName.lenet
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
+
+
+# Practicing by creating my own class, and how to send multiple values in path parameters
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+
+@app.get("/myname/{myname: Person}")
+async def show_name(myname):
+    return {myname}
+
+
+############## Path parameters containing paths ##############
