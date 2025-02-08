7eac98ec4f4269a070b0b0e697813ee1ad098d51
kazigoni <kazimdgoni@gmail.com>
2025-02-08 23:40:19 +0600

    Learned file path parameters. But not clear. after testing the url I got a success but what does it mean. I thought I might be able to download or read the file. Anyway I am not clear about this. Maybe I need to write some python code to read the file

--- main.py
+++ main.py
@@ -77,3 +77,25 @@ async def show_name(myname):
 
 
 ############## Path parameters containing paths ##############
+"""
+Let's say you have a path operation with a path '/files/{file_path}'.
+
+But you need file_path itself to contain a path, like home/johndoe/myfile.txt.
+
+So, the URL for that file would be something like: /files/home/johndoe/myfile.txt
+"""
+# Path convertor¶
+# Using an option directly from Starlette you can declare a path parameter containing a path using a URL like:
+# /files/{file_path:path}
+
+
+@app.get("/files/{file_path:path}")
+async def read_file(file_path: str):
+    return {"file_path": file_path}
+
+"""
+You could need the parameter to contain /home/johndoe/myfile.txt, with a leading slash (/).
+
+In that case, the URL would be: 
+/files//home/johndoe/myfile.txt, with a double slash (//) between files and home.
+"""
